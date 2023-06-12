28086b471c5bd6ad32de5ae0b399ea0fa028ebcc
EC2 Default User <130027255+satoikokoro@users.noreply.github.com>
2023-06-12 10:13:29 +0000

    [Add]public/admin/session

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#アドミンのemailとパスワードです。
+　# rails db:seedを実行後使用いただけます。
+Admin.create!(
+  email: 'admin@admin',
+  password: 'admin0'
+  )
