630ec7f364a62ee336780fdbed4042ca68a57eec
EC2 Default User <130027255+satoikokoro@users.noreply.github.com>
2023-06-15 10:23:57 +0000

    [Add]public/users

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,10 +1,46 @@
 class Public::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update, :resign]
+
+  def index
+    @users = User.all
+  end
+
   def show
+    @user = User.find(params[:id])
+    @cloths = @user.cloths
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
-  def withdraw
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
   end
+
+
+  def resign
+    
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :image)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :clothes, dependent: :destroy
+  has_many :cloths, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
 
   has_one_attached :image
 
-  def get_user_image(width, height)
+  def get_user_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/user_no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
